8d31b7c46fea574cdc0becda4442d5bc329d234d
annan-7 <annanrecord@gmail.com>
2025-07-04 18:22:53 -0400

    pushing updates2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use rusqlite::{params, Connection, Result};
+use serde::{Deserialize, Serialize};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +11,14 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri app");
+}
+#[derive(Serialize, Deserialize)]
+struct Product {
+    id: i32,
+    name: String,
+    price: f64,
 }
