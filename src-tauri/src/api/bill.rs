6a4a9a891bed8a1f4cc50a18b7daf4014e897770
annan-7 <annanrecord@gmail.com>
2025-07-08 21:18:37 -0400

    update

--- src-tauri/src/api/bill.rs
+++ src-tauri/src/api/bill.rs
@@ -4,12 +4,12 @@ use tauri::command;
 
 #[tauri::command]
 pub fn create_bill(items: Vec<BillItem>) -> Result<i64, String> {
-    let mut conn = Connection::open("src-tauri/inventory.db").map_err(|e| e.to_string())?;
+    let mut conn = Connection::open("inventory.db").map_err(|e| e.to_string())?;
     Bill::create(&mut conn, items).map_err(|e| e.to_string())
 }
 
 #[tauri::command]
 pub fn get_bills() -> Result<Vec<Bill>, String> {
-    let conn = Connection::open("src-tauri/inventory.db").map_err(|e| e.to_string())?;
+    let conn = Connection::open("inventory.db").map_err(|e| e.to_string())?;
     Bill::list(&conn).map_err(|e| e.to_string())
 }
