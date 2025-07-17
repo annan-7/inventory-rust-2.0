c0a4bf93bf9e49f5613aaed81b850184931460e0
annan-7 <annanrecord@gmail.com>
2025-07-16 20:41:04 -0400

    update

--- src-tauri/src/api/product.rs
+++ src-tauri/src/api/product.rs
@@ -1,11 +1,13 @@
 use rusqlite::Connection;
 use crate::models::product::Product;
+use crate::models::product::DeletedProduct;
 use tauri::command;
 
+
 #[tauri::command]
-pub fn add_product(name: String, price: f64) -> Result<(), String> {
+pub fn add_product(name: String, price: f64, quantity: i32) -> Result<(), String> {
     let conn = Connection::open("inventory.db").map_err(|e| e.to_string())?;
-    Product::create(&conn, name, price).map_err(|e| e.to_string())
+    Product::create(&conn, name, price, quantity).map_err(|e| e.to_string())
 }
 
 #[tauri::command]
@@ -15,9 +17,9 @@ pub fn get_products() -> Result<Vec<Product>, String> {
 }
 
 #[tauri::command]
-pub fn update_product(id: i32, name: String, price: f64) -> Result<(), String> {
+pub fn update_product(id: i32, name: String, price: f64, quantity: i32) -> Result<(), String> {
     let conn = Connection::open("inventory.db").map_err(|e| e.to_string())?;
-    Product::update(&conn, id, name, price).map_err(|e| e.to_string())
+    Product::update(&conn, id, name, price, quantity).map_err(|e| e.to_string())
 }
 
 #[tauri::command]
@@ -31,3 +33,10 @@ pub fn get_product_by_id_and_name(id: i32, name: String) -> Result<Option<Produc
     let conn = Connection::open("inventory.db").map_err(|e| e.to_string())?;
     Product::get_by_id_and_name(&conn, id, &name).map_err(|e| e.to_string())
 }
+
+#[tauri::command]
+pub fn get_deleted_products() -> Result<Vec<DeletedProduct>, String> {
+    let conn = Connection::open("inventory.db").map_err(|e| e.to_string())?;
+    DeletedProduct::get_all(&conn).map_err(|e| e.to_string())
+}
+
