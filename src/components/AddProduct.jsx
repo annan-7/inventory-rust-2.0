29d4cfce7d223e4f0f74558717a8a901d10d2266
annan-7 <annanrecord@gmail.com>
2025-07-21 20:45:55 -0400

    update

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -62,7 +62,7 @@ export default function AddProduct({ onProductAdded }) {
             id="productName"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder="Enter product name"
+            placeholder="Enter product the name"
             disabled={loading}
             required
             className="bg-black-600 text-white border border-gray-600 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
