4c29df8013ece810653fe21a6de7e7ba65d814cf
annan-7 <annanrecord@gmail.com>
2025-07-23 21:36:43 -0400

    Update

--- src/components/DeleteProductsList.jsx
+++ src/components/DeleteProductsList.jsx
@@ -64,6 +64,7 @@ export default function DeletedProductsList() {
               <span className="text-gray-300">Página {page} de {totalPages}</span>
               <button onClick={() => setPage(page + 1)} disabled={page === totalPages} className="px-3 py-1 rounded bg-gray-700 text-white disabled:opacity-50">Siguiente</button>
             </div>
+            
           )}
         </>
       )}
