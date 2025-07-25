48728dd1a16f28d14d138aab235352802afe83db
annan-7 <annanrecord@gmail.com>
2025-07-24 20:16:44 -0400

    update

--- src-tauri/src/api/bill.rs
+++ src-tauri/src/api/bill.rs
@@ -2,11 +2,14 @@ use rusqlite::Connection;
 use crate::models::bill::{Bill, BillItem};
 use tauri::command;
 
+
 #[tauri::command]
 pub fn create_bill(items: Vec<BillItem>) -> Result<i64, String> {
     let mut conn = Connection::open("inventory.db").map_err(|e| e.to_string())?;
     Bill::create(&mut conn, items).map_err(|e| e.to_string())
 }
+
+
 
 #[tauri::command]
 pub fn get_bills() -> Result<Vec<Bill>, String> {
