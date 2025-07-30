29fefa65ee1d4df14b3e7509263a0205a80b48a7
annan-7 <annanrecord@gmail.com>
2025-07-29 20:43:57 -0400

    update

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -70,6 +70,9 @@ export default function AddProduct({ onProductAdded }) {
             className="bg-gray-800 text-white border border-gray-600 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
+
+
+        
         <div className="flex flex-col gap-1">
           <label htmlFor="productPrice" className="font-semibold">Precio ($):</label>
           <input
