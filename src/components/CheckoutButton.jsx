5290b3eb3ecb02160b8bdaec81b5f7f380e6d211
annan-7 <annanrecord@gmail.com>
2025-07-30 21:11:51 -0400

    update

--- src/components/CheckoutButton.jsx
+++ src/components/CheckoutButton.jsx
@@ -15,7 +15,7 @@ export default function CheckoutButton({ cart, clearCart }) {
       
       clearCart();
     } catch (err) {
-      alert("Error: " + err);
+      alert("Error  : " + err);
     }
   };
 
